e7270fe228abc84f17a05868802d132fdb6fd177
Shield Koh Yee Sing Daymas <s10265961@connect.np.edu.sg>
2025-01-16 15:25:51 +0800

    method

--- PRG2-T13-07/Airline.cs
+++ PRG2-T13-07/Airline.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -27,5 +28,15 @@ namespace PRG2_T13_07
             Code = c;
             Flights = f;
         }
+         public bool AddAirline (Airline airline)
+        {
+            if (airline == null)
+            {
+                return false;
+            }
+            airline.Add(airline.Code, airline);
+            return true;
+        }
+        public AddBoardingGate 
     }
 }
